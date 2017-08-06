b60217ec11ef504d68175eb49a89876c126440f7
Zachary Fregin <zach.fregin@gmail.com>
2017-08-05 21:36:30 -0700

    Cleanup
    Revised version with code cleaned up

--- Python/daily_txt_transfer.py
+++ Python/daily_txt_transfer.py
@@ -12,26 +12,18 @@ print("Welcome to the Daily .txt File Mover!")
 source  = str(input("Please enter your source file directory: "))
 dest = str(input("Please enter your destination file directory: "))
 
-def txt_move(path, dest):
-	# # Checks if filename indicates a .txt file
-	# if fnmatch.fnmatch(file, '*.txt'):
-	# 	# Joins root path with file name
-	# 	file_path = os.path.join(root,file)
-	# 	print(file_path)
-	# 	# Moves file to destination directory
-	shutil.copy(path,dest)
-		# os.remove(file_path)
-
 def recent_changes(source, dest):
+	# Walks through tree of source directory
 	for root,directories,filenames in os.walk(source):
 		for file in filenames:
 			# Checks if filename indicates a .txt file
 			if fnmatch.fnmatch(file, '*.txt'):
 				# Joins root path with file name
 				file_path = os.path.join(root,file)
+				# Checks if file was modified within last 24 hours
 				if os.path.getmtime(file_path) > previous_24hr:
 					print(file_path)
-					txt_move(file_path,dest)
+					shutil.copy(file_path,dest)
 
 recent_changes(source,dest)
 
