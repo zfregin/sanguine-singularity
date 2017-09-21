decbea0e263ba482adf1f8e6fea2c5686f2ce5d2
Zachary Fregin <zach.fregin@gmail.com>
2017-09-20 19:50:35 -0700

    Revised program
    Better case handling for input textboxes. No error when trying to parse
    empty textbox for height and width.

--- C#/PostageCalculatorChallenge/PostageCalculatorChallenge/Default.aspx
+++ C#/PostageCalculatorChallenge/PostageCalculatorChallenge/Default.aspx
@@ -12,20 +12,20 @@
             Postage Calculator<br />
             <br />
             Width:&nbsp;
-            <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="widthTextBox_TextChanged"></asp:TextBox>
+            <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="handleChange"></asp:TextBox>
             <br />
             Height:&nbsp;
-            <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="heightTextBox_TextChanged"></asp:TextBox>
+            <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="handleChange"></asp:TextBox>
             <br />
             Length:&nbsp;
-            <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="lengthTextBox_TextChanged"></asp:TextBox>
+            <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="handleChange"></asp:TextBox>
             <br />
             <br />
-            <asp:RadioButton ID="groundRadio" runat="server" GroupName="Shipping Radio" OnCheckedChanged="groundRadio_CheckedChanged" Text="Ground" AutoPostBack="True" />
+            <asp:RadioButton ID="groundRadio" runat="server" GroupName="Shipping Radio" OnCheckedChanged="handleChange" Text="Ground" AutoPostBack="True" />
             <br />
-            <asp:RadioButton ID="airRadio" runat="server" GroupName="Shipping Radio" OnCheckedChanged="airRadio_CheckedChanged" Text="Air" AutoPostBack="True" />
+            <asp:RadioButton ID="airRadio" runat="server" GroupName="Shipping Radio" OnCheckedChanged="handleChange" Text="Air" AutoPostBack="True" />
             <br />
-            <asp:RadioButton ID="nextDayRadio" runat="server" GroupName="Shipping Radio" OnCheckedChanged="nextDayRadio_CheckedChanged" Text="Next Day" AutoPostBack="True" />
+            <asp:RadioButton ID="nextDayRadio" runat="server" GroupName="Shipping Radio" OnCheckedChanged="handleChange" Text="Next Day" AutoPostBack="True" />
             <br />
             <br />
             <asp:Label ID="resultLabel" runat="server"></asp:Label>
--- C#/PostageCalculatorChallenge/PostageCalculatorChallenge/Default.aspx.cs
+++ C#/PostageCalculatorChallenge/PostageCalculatorChallenge/Default.aspx.cs
@@ -9,57 +9,56 @@ namespace PostageCalculatorChallenge
 {
     public partial class Default : System.Web.UI.Page
     {
-        private float getDimensions()
+        private void performCalc()
         {
-            float width = float.Parse(widthTextBox.Text);
-            float height = float.Parse(heightTextBox.Text);
-            //float length = float.TryParse(lengthTextBox.Text);
-            if (lengthTextBox.Text == "")
-            {
-                return getVolume(width, height);
-            }
-            else
-            {
-                float length = float.Parse(lengthTextBox.Text);
-                return getVolume(width, height, length);
-            }
-
+            // Check if values in textbox/radios
+            if (!valuesExist()) return;
+            // What is the volume of the package?
+            float volume = 0;
+            if (!tryGetVolume(out volume)) return;
+            // What is the cost of the shipment?
+            double cost = volume * getMultiplier();
+            // Display info to user
+            resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship", cost);
         }
 
-        private float getVolume(float width, float height, float length = 1)
+        private bool valuesExist()
         {
-            float volume = width * height * length;
+            // See if radio buttons selected
+            if (!groundRadio.Checked
+                && !airRadio.Checked
+                && !nextDayRadio.Checked)
+                return false;
+            // See if textboxes are empty
+            if (widthTextBox.Text.Trim().Length == 0
+                | heightTextBox.Text.Trim().Length == 0)
+                return false;
 
-            return volume;
+            return true;
+        }
+
+        private bool tryGetVolume(out float volume)
+        {
+            float width = 0;
+            float height = 0;
+            float length = 0;
+            volume = 0;
+            // If able to parse float value from textbox, return value, otherwise do nothing
+            if (!float.TryParse(widthTextBox.Text.Trim(), out width)) return false;
+            if (!float.TryParse(heightTextBox.Text.Trim(), out height)) return false;
+            // If able to parse float value from textbox, return value, otherwise set length to 1
+            if (!float.TryParse(lengthTextBox.Text.Trim(), out length)) length = 1;
+
+            volume = width * height * length;
+            return true;
         }
 
         private float getMultiplier()
         {
-            if (groundRadio.Checked)
-            {
-                return getDimensions() * 0.15f;
-            }
-            else if (airRadio.Checked)
-            {
-                return getDimensions() * 0.25f;
-            }
-            else if (nextDayRadio.Checked)
-            {
-                return getDimensions() * 0.45f;
-            }
+            if (groundRadio.Checked) return 0.15f;
+            else if (airRadio.Checked) return 0.25f;
+            else if (nextDayRadio.Checked) return 0.45f;
             else return 0;
-        }
-
-        private void labelMessage()
-        {
-            if (((widthTextBox.Text == "") |
-               (widthTextBox.Text == "0")) |
-               ((heightTextBox.Text == "") |
-               (heightTextBox.Text == "0")))
-            {
-                resultLabel.Text = "Please enter a nonzero width and height";
-            } else
-            resultLabel.Text = string.Format("Your parcel will cost {0:C} to ship", getMultiplier());
         }
 
         protected void Page_Load(object sender, EventArgs e)
@@ -67,34 +66,9 @@ namespace PostageCalculatorChallenge
 
         }
 
-        protected void groundRadio_CheckedChanged(object sender, EventArgs e)
+        protected void handleChange(object sender, EventArgs e)
         {
-            labelMessage();
-        }
-
-        protected void airRadio_CheckedChanged(object sender, EventArgs e)
-        {
-            labelMessage();
-        }
-
-        protected void nextDayRadio_CheckedChanged(object sender, EventArgs e)
-        {
-            labelMessage();
-        }
-
-        protected void widthTextBox_TextChanged(object sender, EventArgs e)
-        {
-            labelMessage();
-        }
-
-        protected void heightTextBox_TextChanged(object sender, EventArgs e)
-        {
-            labelMessage();
-        }
-
-        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
-        {
-            labelMessage();
+            performCalc();
         }
     }
 }
