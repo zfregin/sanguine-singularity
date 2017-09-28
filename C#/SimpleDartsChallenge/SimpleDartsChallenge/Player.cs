62f3d65a23d295d5950d982f7ceb1a37dd1af126
Zachary Fregin <zach.fregin@gmail.com>
2017-09-28 16:53:33 -0700

    Simple Darts Challenge

---
+++ C#/SimpleDartsChallenge/SimpleDartsChallenge/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleDartsChallenge
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
