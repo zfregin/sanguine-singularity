c19ad5c930789c421634ae27879d45bf7ac27cb0
Zachary Fregin <zach.fregin@gmail.com>
2017-09-29 12:11:04 -0700

    Student Course Challenge

---
+++ C#/ChallengeStudentCourses/ChallengeStudentCourses/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeStudentCourses
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Grades> Grades{ get; set; }
+    }
+}
