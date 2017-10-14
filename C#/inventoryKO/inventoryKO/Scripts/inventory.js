fd98ca685bf2734d73f229b5bd5f0c2919f12d4f
Zachary Fregin <zach.fregin@gmail.com>
2017-10-14 15:49:24 -0700

    Functioning Simple Grid!!!
    It really works!!

--- C#/inventoryKO/inventoryKO/Scripts/inventory.js
+++ C#/inventoryKO/inventoryKO/Scripts/inventory.js
@@ -8,35 +8,6 @@
     { name: "Fidget Spinner", inventory: 623, price: 8.00 }
 ];
 
-/*
-var PagedGridModel = function (items) {
-    this.items = ko.observableArray(items);
-
-    this.addItem = function () {
-        this.items.push({ name: "new item", inventory: 5, price: 20 });
-    };
-
-    this.sortByName = function () {
-        this.items.sort(function (a, b) {
-            return a.name < b.name ? -1 : 1;
-        });
-    };
-
-    this.jumpToFirstPage = function () {
-        this.gridViewModel.currentPageIndex(0);
-    };
-
-    this.gridViewModel = new ko.simpleGrid.viewModel({
-        data: this.items,
-        columns: [
-            { headerText: "Item Name", rowText: "name" },
-            { headerText: "Inventory", rowText: "inventory" },
-            { headerText: "Price", rowText: function (item) { return "$" + item.price.toFixed(2) } }
-        ],
-        pageSize: 4
-    });
-};
-*/
 
 var PagedGridModel = function (items) {
     this.items = ko.observableArray(items);
